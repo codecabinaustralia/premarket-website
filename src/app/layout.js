84c56ee91330643c655e952cbfe8383c8d2fac10
Josh Reilly <reilly@MacBookAir.modem>
2025-07-20 17:03:02 +1000

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,35 +1,33 @@
 import { Poppins, Inter, Anton } from "next/font/google";
-
 import "./globals.css";
-
 
 const poppins = Poppins({
   weight: ['400', '500', '600', '700', '800'],
+  subsets: ['latin'],
   variable: '--font-poppins',
 });
 
 const inter = Inter({
   weight: ['400', '500', '600', '700', '800'],
+  subsets: ['latin'],
   variable: '--font-inter',
 });
 
 const antons = Anton({
   weight: ['400'],
+  subsets: ['latin'],
   variable: '--font-anton',
 });
 
-
 export const metadata = {
   title: "Premarket",
-  description: "Social Network for Australia Sell smarter. See who’s interested before you list.",
+  description: "Social Network for Australia. Sell smarter. See who’s interested before you list.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${antons.variable} antialiased`}
-      >
+      <body className={`${antons.variable} antialiased`}>
         {children}
       </body>
     </html>
