0ae5c32590dd0cadf4d0bb711df438e581ec170f
Josh Reilly <josh@codecabin.com.au>
2025-11-07 18:22:01 +1100

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Poppins, Inter, Anton } from "next/font/google";
 import { ModalProvider } from './context/ModalContext';
+import Script from 'next/script';
 
 import "./globals.css";
 
@@ -23,7 +24,7 @@ const antons = Anton({
 
 export const metadata = {
   title: "Premarket",
-  description: "Social Network for Australia. Sell smarter. See who’s interested before you list.",
+  description: "Social Network for Australia. Sell smarter. See who's interested before you list.",
 };
 
 export default function RootLayout({ children }) {
@@ -33,6 +34,25 @@ export default function RootLayout({ children }) {
         <ModalProvider>
           {children}
         </ModalProvider>
+
+        {/* Tawk.to Live Chat Script */}
+        <Script
+          id="tawk-to-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/690d9d7462ef6419564dc9b1/1j9eiu4lf';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
