905838347ca3d30bad36ee19ed712a91360d0775
Josh Reilly <josh@codecabin.com.au>
2025-11-13 08:20:04 +1100

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,19 +24,38 @@ const antons = Anton({
 
 export const metadata = {
   title: "Premarket",
-  description: "Social Network for Australia. Sell smarter. See who's interested before you list.",
+  description: "Giving the confidence to home owners by collecting real buyer interest and price opinions before going to market. Smart real estate in Australia.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PS65V87');
+          `}
+        </Script>
+      </head>
       <body className={`${antons.variable} antialiased bg-white`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PS65V87"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        
         <ModalProvider>
           {children}
         </ModalProvider>
-
-        
-        
       </body>
     </html>
   );
